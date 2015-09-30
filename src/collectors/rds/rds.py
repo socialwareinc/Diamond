5227a90c80ced665dfe88a54ae7cb10f9dbfddb2
Michael Fox <mfox@socialware.com>
2015-09-30 14:12:36 -0500

    Removed unused memoized decorator

--- src/collectors/rds/rds.py
+++ src/collectors/rds/rds.py
@@ -50,40 +50,6 @@ try:
 except ImportError:
     boto3 = False
 
-class memoized(object):
-    """Decorator that caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned, and
-    the function is not re-evaluated.
-
-    Based upon from http://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
-    Nota bene: this decorator memoizes /all/ calls to the function.  For
-    a memoization decorator with limited cache size, consider:
-    bit.ly/1wtHmlM
-    """
-
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args, **kwargs):
-        # If the function args cannot be used as a cache hash key, fail fast
-        key = cPickle.dumps((args, kwargs))
-        try:
-            return self.cache[key]
-        except KeyError:
-            value = self.func(*args, **kwargs)
-            self.cache[key] = value
-            return value
-
-    def __repr__(self):
-        """Return the function's docstring."""
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        """Support instance methods."""
-        return functools.partial(self.__call__, obj)
-
-
 def utc_to_local(utc_dt):
     """
     :param utc_dt: datetime in UTC
@@ -103,7 +69,7 @@ class RdsCollector(diamond.collector.Collector):
         'MetricInfo',
         'name aws_type diamond_type precision default_to_zero')
 
-    # AWS metrics for ELBs
+    # AWS metrics for RDS
     metrics = [
         MetricInfo('BinLogDiskUsage', 'Average', 'GAUGE', 0, False),
         MetricInfo('CPUUtilization', 'Average', 'GAUGE', 2, False),
